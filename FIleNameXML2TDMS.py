de6b89f251f2de69eda52d9d3399366c9c50fd20
WilliamleeIao <wilee_6@hotmail.com>
2022-11-11 14:18:51 +0800

    Editing new code

--- FIleNameXML2TDMS.py
+++ FIleNameXML2TDMS.py
@@ -12,7 +12,6 @@ class XML2TDMS(IFileName):
     def input_filename(self,in_file):
       self.__input_filename = in_file
 
-    
     @property
     def output_filename(self):
      return self.__output_filename
@@ -20,6 +19,15 @@ class XML2TDMS(IFileName):
     @input_filename.setter
     def output_filename(self,out_file):
      self.__output_filename = out_file
+     
+     
+    @property
+    def exe_cmd_filename(self):
+     return self.__execution_filename
+    
+    @exe_cmd_filename.setter
+    def exe_cmd_filename(self,cmd_path):
+     self.__execution_filename = cmd_path
      
     def construct_process_str(self) -> str:
         return  "{} -c \"{}\" \"{}\" ".format(self.ExecutionFilePath, self.__input_filename,self.__output_filename)
