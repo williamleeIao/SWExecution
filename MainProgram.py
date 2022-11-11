6b0bfc1a1a8fd6fe9e231b56a5269cd40a1bdab0
WilliamleeIao <wilee_6@hotmail.com>
2022-11-11 15:08:58 +0800

    Changing the code to add in IFileHandler

--- MainProgram.py
+++ MainProgram.py
@@ -1,13 +1,11 @@
-from SWOperation import ISWoperation
-from FIleName import IFileName
-from DeCompress import TDR
-from FIleNameXML2TDMS import XML2TDMS
+from SW_Execution.SWOperation import ISWoperation
+from SW_Execution.FIleName import IFileName
+from SW_Execution.DeCompress import TDR
+from SW_Execution.FIleNameXML2TDMS import XML2TDMS
 import ntpath
 
 #Upper level test code
 def main_program_process_flow(tdr : ISWoperation, fileinstances : IFileName):
-    fileinstances.input_filename =""
-    fileinstances.exe_cmd_filename =""
     tdr.RunCommand(fileinstances)
 
 def process_file_name(filepath:str) ->str:
@@ -15,12 +13,25 @@ def process_file_name(filepath:str) ->str:
     XML_path_name = XML_path_name.replace("_COMPRESSED.tdr", ".xml")
     return XML_path_name
 
-if __name__ == "__main__":
-    
-    file_path = "" 
-    tdr = TDR()
+def filehandler(filepath:str, exe_cmd:str) ->IFileName:
+    """_summary_
+       This function is use for assign the file name into the variable that use in tdr 
+
+    Args:
+        filepath (str): _description_
+        exe_cmd (str): _description_
+
+    Returns:
+        IFileName: IFileName use in the SWOperation program
+    """    
     compress_process = XML2TDMS()
-    compress_process.input_filename = file_path
-    compress_process.output_filename = process_file_name(file_path)
-    compress_process.exe_cmd_filename = "some path"
+    compress_process.input_filename = filepath
+    compress_process.output_filename = process_file_name(filepath)
+    compress_process.exe_cmd_filename = exe_cmd
+    return compress_process
+
+if __name__ == "__main__":  
+    file_path = r"C:\Users\willlee\Desktop\MICR9902102PEN_134435C-01L_2332_11-2-2022_4-13-26_8876_COMPRESSED.tdr" 
+    tdr = TDR()
+    compress_process = filehandler(file_path,"some_cmd")
     main_program_process_flow(tdr,compress_process)
